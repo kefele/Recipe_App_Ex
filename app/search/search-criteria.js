c494a60dd236f0de370a2c51fb2154d1418aa734
Jonathon Branham <kefele@aim.com>
2018-08-23 01:58:45 -0500

    funtional recs. needs styling

--- app/search/search-criteria.js
+++ app/search/search-criteria.js
@@ -18,33 +18,33 @@ const searchCriteria = {
     </select>
    
     <button ng-click="$ctrl.search($ctrl.ingredientOne, $ctrl.excludes, $ctrl.diet)">Search for Recipes</button>
-    <p>Search Component</p>
+    
     <ul>
         <li  ng-repeat="item in $ctrl.recipes" track by $index>
-        <p>{{item.recipe.label}}<button ng-click="$ctrl.add(item.recipe.label)">Add</button></p>
+            <p>{{item.recipe.label}}<button ng-click="$ctrl.add(item.recipe.label)">Add</button></p>
 
-        <img src="{{item.recipe.image}}" >
-        <a href="{{item.recipe.url}}">link</a>
-        <p ng-repeat="list in item.recipe.ingredientLines"track by $index>{{list}}<p>            
+            <img src="{{item.recipe.image}}" >
+            <a href="{{item.recipe.url}}">link</a>
+            <p ng-repeat="list in item.recipe.ingredientLines"track by $index>{{list}}</p>            
         </li>
     </ul>
     `,
     controller: ["SearchService", "$location", function (SearchService, $location) {
         const vm = this;
-        vm.search = (ingredientOne, excludes,diet) => {
-            SearchService.recipeSearch(ingredientOne, excludes,diet).then((response) => {
+        vm.search = (ingredientOne, excludes, diet) => {
+            SearchService.recipeSearch(ingredientOne, excludes, diet).then((response) => {
                 console.log(response);
                 vm.recipes = response.data.hits;
-               
-         });
-           
+
+            });
+
         }
-        vm.add=(newItem)=>{
+        vm.add = (newItem) => {
             SearchService.setFavFood(newItem);
             $location.path("/favorites-page")
         }
 
-   
+
     }]
 
 };
