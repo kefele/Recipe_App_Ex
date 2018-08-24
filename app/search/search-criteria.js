85eaeddea3edf79dd322df7b0598ba3f93862db0
Jonathon Branham <kefele@aim.com>
2018-08-23 22:32:52 -0500

    attempt at hiding ingredients

--- app/search/search-criteria.js
+++ app/search/search-criteria.js
@@ -1,35 +1,38 @@
 "use strict"
 const searchCriteria = {
-    template: `
+    template: 
+    `
     
-    <div id="searchPage">
-        <label>Ingredients</label>
-        <input type ="text" ng-model="$ctrl.ingredientOne">
-        <label>Exclude Ingredients: </label>
-        <input type="text" ng-model="$ctrl.excludes">
-    
-        select a diet:
+        <div id="searchPage">
+            <label>Ingredients</label>
+            <input type ="text" ng-model="$ctrl.ingredientOne">
+            <label>Exclude Ingredients: </label>
+            <input type="text" ng-model="$ctrl.excludes">
+        
+            select a diet:
 
-        <select ng-model="$ctrl.diet">
-        <option value="balanced">Balanced
-        <option value="high-protein">High Protein
-        <option value="high-fiber">High Fiber
-        <option value="low-fat">Low Fat
-        <option value="low-carb">Low Carb
-        </select>   
-        <button ng-click="$ctrl.search($ctrl.ingredientOne, $ctrl.excludes, $ctrl.diet)">Search for Recipes</button>
-    </div>
-    <ul>
-        <li class="data"  ng-repeat="item in $ctrl.recipes" track by $index>
-            <p>{{item.recipe.label}}<button ng-click="$ctrl.add(item.recipe.label)">Add</button></p>
+            <select ng-model="$ctrl.diet">
+                <option value="balanced">Balanced
+                <option value="high-protein">High Protein
+                <option value="high-fiber">High Fiber
+                <option value="low-fat">Low Fat
+                <option value="low-carb">Low Carb
+            </select>   
+            <button ng-click="$ctrl.search($ctrl.ingredientOne, $ctrl.excludes, $ctrl.diet)">Search for Recipes</button>
+        </div>
+        
+            <ul>
+                <li class="data"  ng-repeat="item in $ctrl.recipes" track by $index>
+                    <p>{{item.recipe.label}}</p>
 
-            <img src="{{item.recipe.image}}" >
-            <a href="{{item.recipe.url}}" target="_blank">link</a>
-            <p ng-repeat="list in item.recipe.ingredientLines"track by $index>{{list}}</p>            
-        </li>
-    </ul>
-    </div>
-    
+                    <img src="{{item.recipe.image}}" >
+                    <button ng-click="$ctrl.ingredients(1)">Ingredients</button>
+                    <a href="{{item.recipe.url}}" target="_blank">link</a>
+                    <button ng-click="$ctrl.add(item.recipe.label)">Add</button>
+                    <p ng-show="disIngredients ==1" ng-repeat="list in item.recipe.ingredientLines"track by $index>{{list}}</p>            
+                </li>
+            </ul>
+ 
     `,
     controller: ["SearchService", "$location", function (SearchService, $location) {
         const vm = this;
@@ -44,6 +47,10 @@ const searchCriteria = {
         vm.add = (newItem) => {
             SearchService.setFavFood(newItem);
             $location.path("/favorites-page")
+        }
+        vm.ingredients = (num) => {
+            let disIngredients = num;
+            return disIngredients;
         }
 
 
