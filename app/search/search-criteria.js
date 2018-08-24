f91257d90c986a5cadc00d49b2a5fa8516be8b49
Jonathon Branham <kefele@aim.com>
2018-08-23 20:09:58 -0500

    first attempt at style

--- app/search/search-criteria.js
+++ app/search/search-criteria.js
@@ -1,6 +1,8 @@
 "use strict"
 const searchCriteria = {
     template: `
+    <div id="body">
+    <div id="searchPage">
     <label>Ingredients</label>
     <input type ="text" ng-model="$ctrl.ingredientOne">
     <label>Exclude Ingredients: </label>
@@ -8,26 +10,28 @@ const searchCriteria = {
     select a diet:
    
 
-<select ng-model="$ctrl.diet">
+        <select ng-model="$ctrl.diet">
         <option value="balanced">Balanced
         <option value="high-protein">High Protein
         <option value="high-fiber">High Fiber
         <option value="low-fat">Low Fat
         <option value="low-carb">Low Carb
         
-    </select>
+        </select>
    
     <button ng-click="$ctrl.search($ctrl.ingredientOne, $ctrl.excludes, $ctrl.diet)">Search for Recipes</button>
-    
+    </div>
     <ul>
-        <li  ng-repeat="item in $ctrl.recipes" track by $index>
+        <li class="data"  ng-repeat="item in $ctrl.recipes" track by $index>
             <p>{{item.recipe.label}}<button ng-click="$ctrl.add(item.recipe.label)">Add</button></p>
 
             <img src="{{item.recipe.image}}" >
-            <a href="{{item.recipe.url}}">link</a>
+            <a href="{{item.recipe.url}}" target="_blank">link</a>
             <p ng-repeat="list in item.recipe.ingredientLines"track by $index>{{list}}</p>            
         </li>
     </ul>
+    </div>
+    
     `,
     controller: ["SearchService", "$location", function (SearchService, $location) {
         const vm = this;
